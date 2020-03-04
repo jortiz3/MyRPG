57e16564d845d281ffd2ba3e9b6b32b067a19a73
jortiz3 <justinwortiz7@gmail.com>
2020-03-04 14:27:54 -0500

    Update SceneryObject.cs
    -fix: scenery objects now properly move off of structures on area load; they also properly delete when structure placed on top

--- Assets/Scripts/SceneryObject.cs
+++ Assets/Scripts/SceneryObject.cs
@@ -5,47 +5,41 @@ using AreaManagerNS;
 /// <summary>
 /// Written by Justin Ortiz
 /// </summary>
-public class SceneryObject : MonoBehaviour
-{
+public class SceneryObject : MonoBehaviour {
+
 	private void Start() {
 		transform.SetParent(AreaManager.GetEntityParent("Scenery"));
 	}
 
-	private void OnTriggerEnter(Collider other) {
+	private void OnTriggerStay(Collider other) {
 		if (other.CompareTag("structure")) { //overlapping with structure
 			if (StructureGridManager.instance.EditFinalizing) { //if the player is finalizing a structure
 				Destroy(gameObject); //destroy this object
 			} else if (!StructureGridManager.instance.EditEnabled) { //if player isn't moving the structure around in edit mode >> game is loading an area
-				Vector3 newPos = transform.position + (Vector3.up * 3f); //try to move object up
+				Vector3 newPos; //stores the potential new position
 				bool newPosFound = false;
-				if (newPos.y < AreaManager.AreaSize) { //if new position is in bounds
-					transform.position = newPos; //set the position
-					newPosFound = true; //stop checking
-				}
+				float distanceScale = 10f;
 
-				if (!newPosFound) { //if prev check didn't work
-					newPos = transform.position + (Vector3.left * 3f); //try left
-					if (newPos.x > -AreaManager.AreaSize) { //if in bounds
-						transform.position = newPos; //set the position
-						newPosFound = true; //stop checking
-					}
+				for (int x = -1; x < 2; x++) { //x to be used for direction
+					for (int y = -1; y < 2; y++) { //y to be used for direction
+						newPos = transform.position + (new Vector3(x, y, 0) * distanceScale); //get the potential new position
 
-					if (!newPosFound) { //if prev check didn't work
-						newPos = transform.position + (Vector3.right * 3f); //try right
-						if (newPos.x < AreaManager.AreaSize) { //if in bounds
-							transform.position = newPos; //set the position
-							newPosFound = true; //stop checking
-						}
-
-						if (!newPosFound) { //if prev check didn't work
-							newPos = transform.position + (Vector3.down * 3f); //try down
-							if (newPos.y > -AreaManager.AreaSize) { //if in bounds
+						if (newPos.y < AreaManager.AreaSize && -AreaManager.AreaSize < newPos.y) { //if the y is in bounds
+							if (newPos.x < AreaManager.AreaSize && -AreaManager.AreaSize < newPos.x) { //if the x is in bounds
 								transform.position = newPos; //set the position
-							} else { //no positions suitable; technically not possible, but just in case
-								Destroy(gameObject);
+								newPosFound = true; //allows us to break out of outer loop
+								break; //stop processing y
 							}
 						}
 					}
+
+					if (newPosFound) { //if we have a new pos
+						break; //stop processing x
+					}
+				}
+
+				if (!newPosFound) { //if made it through both loops without new pos
+					Destroy(gameObject); //just delete object
 				}
 			}
 		}
